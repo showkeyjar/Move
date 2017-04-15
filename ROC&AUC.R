e053125438a2e97f5dc72ec3e0972e377ef86ece
blueliberty <blue_liberty@qq.com>
2017-04-15 22:43:01 +0800

    绘制ROC&计算AUC

---
+++ ROC&AUC.R
@@ -0,0 +1,2 @@
+modelroc<-roc(aSAH$outcome, aSAH$s100b,thresholds = 0.5) # aSAH$outcome-real value; aSAH$s100b-prediction
+plot(modelroc,print.auc=T,auc.polygon=T,grid=c(0.1,0.2),grid.col=c('green','red'),max.auc.polygon=T,auc.polygon.col='skyblue',print.thres=T)
