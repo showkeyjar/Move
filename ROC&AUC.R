ea9fa38d31d747516401302940439e2d911429a3
blueliberty <blue_liberty@qq.com>
2017-04-17 00:01:12 +0800

    4-17

--- ROC&AUC.R
+++ ROC&AUC.R
@@ -1,2 +1,3 @@
+library(pROC)
 modelroc<-roc(aSAH$outcome, aSAH$s100b,thresholds = 0.5) # aSAH$outcome-real value; aSAH$s100b-prediction
 plot(modelroc,print.auc=T,auc.polygon=T,grid=c(0.1,0.2),grid.col=c('green','red'),max.auc.polygon=T,auc.polygon.col='skyblue',print.thres=T)
--- dummies.R
+++ dummies.R
@@ -1,5 +1,4 @@
 library(dummies)
 
-test_data <- data.frame('gender' = c('F', 'M'), 'flightclass' = c(1, 2))
-combi <- dummy.data.frame(test_data, names = c('gender', 'flightclass'), sep='_')
-combi
+(test_data <- data.frame('gender' = c('F', 'M'), 'flightclass' = c(1, 2)))
+(combi <- dummy.data.frame(test_data, names = c('gender', 'flightclass'), sep='_'))
